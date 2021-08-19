0d7560371507de651e4c54cf58de2f1dc6116593
Chinmay Mehta <1234.dsp.ds@gmail.com>
2021-08-19 18:13:27 +0530

    some changes

--- src/app/components/side-nav-bar/side-nav-bar.component.ts
+++ src/app/components/side-nav-bar/side-nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav-bar',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(router:Router) { 
+    router.navigate(['list']);
+  }
 
   ngOnInit(): void {
+  
   }
 
 }
